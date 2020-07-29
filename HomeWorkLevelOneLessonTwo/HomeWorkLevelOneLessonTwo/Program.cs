687aa96af863c7684a97866427fb63eb8340836d
kamilgaliev <kamil11122012@gmail.com>
2020-07-29 23:47:02 +0300

    создаа программа возвращающая минимальное из трёх чисел

--- HomeWorkLevelOneLessonTwo/HomeWorkLevelOneLessonTwo/Program.cs
+++ HomeWorkLevelOneLessonTwo/HomeWorkLevelOneLessonTwo/Program.cs
@@ -4,12 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HomeWorkLevelOneLessonTwo
+namespace MinFromThird
 {
     class Program
     {
+        /*Галиев Камиль
+         * Написать метод, возвращающий минимальное из трёх чисел.
+         */
+
+        public static int MyMin(int a, int b, int c)
+        {
+
+            if (a <= b && a <= c)
+            {
+                return a;
+            }
+            else if (b <= c)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Минимум из трех чисел\n");
+
+            Console.WriteLine("Введите первое число");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите третье число");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"\nМинимальное число из: {a}, {b}, {c} - это {MyMin(a,b,c)}");
+
+            Console.ReadLine();
         }
     }
 }
