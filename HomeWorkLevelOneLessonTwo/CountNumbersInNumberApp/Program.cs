4876090a73adedc3192fc76e8cc56df551adc69a
kamilgaliev <kamil11122012@gmail.com>
2020-07-30 23:35:14 +0300

    Добавлена программа метод подсчета количества цифр числа

---
+++ HomeWorkLevelOneLessonTwo/CountNumbersInNumberApp/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountNumbersInNumberApp
+{
+    class Program
+    {
+        /*
+         * Галиев Камиль
+         * 
+         * Написать метод подсчета количества цифр числа
+         */
+
+        public static bool IsNumber(string number)
+        {
+            int num;
+            if (Int32.TryParse(number,out num))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        public static int CountNumbers(int number)
+        {
+            int count = 0;
+
+            do
+            {
+                count++;
+                number = number / 10;
+            }
+            while (number != 0);
+
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            int MyNumber = -1;
+
+            while(MyNumber < 0)
+            {
+                Console.WriteLine("Введите положительное число");
+                string number = Console.ReadLine();
+
+                if (IsNumber(number))
+                {
+                    MyNumber = Convert.ToInt32(number);
+
+                    int count = CountNumbers(MyNumber);
+                    if (count == 1)
+                    {
+                        Console.WriteLine($"Число {MyNumber} состоит из {count} числа");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Число {MyNumber} состоит из {count} чисел");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели букву или слово\n");
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ HomeWorkLevelOneLessonTwo/CountNumbersInNumberApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("CountNumbersInNumberApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CountNumbersInNumberApp")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("692caa98-e7a0-4473-9f63-1af925cf00e4")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
