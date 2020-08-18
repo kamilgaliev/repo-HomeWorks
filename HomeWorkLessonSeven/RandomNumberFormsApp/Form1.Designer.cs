43063eaf417182dfeaada232e68829485dde963e
kamilgaliev <kamil11122012@gmail.com>
2020-08-18 12:27:19 +0300

    Домашняя работа по 7 уроку. Угадай число

---
+++ HomeWorkLessonSeven/RandomNumberFormsApp/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace RandomNumberFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxForNum = new System.Windows.Forms.TextBox();
+            this.btnCheckNum = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(263, 285);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(387, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите число";
+            // 
+            // txtBoxForNum
+            // 
+            this.txtBoxForNum.Location = new System.Drawing.Point(390, 48);
+            this.txtBoxForNum.Name = "txtBoxForNum";
+            this.txtBoxForNum.Size = new System.Drawing.Size(78, 20);
+            this.txtBoxForNum.TabIndex = 2;
+            // 
+            // btnCheckNum
+            // 
+            this.btnCheckNum.Location = new System.Drawing.Point(490, 48);
+            this.btnCheckNum.Name = "btnCheckNum";
+            this.btnCheckNum.Size = new System.Drawing.Size(45, 20);
+            this.btnCheckNum.TabIndex = 3;
+            this.btnCheckNum.Text = "ОК";
+            this.btnCheckNum.UseVisualStyleBackColor = true;
+            this.btnCheckNum.Click += new System.EventHandler(this.btnCheckNum_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 310);
+            this.Controls.Add(this.btnCheckNum);
+            this.Controls.Add(this.txtBoxForNum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Угадай число";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxForNum;
+        private System.Windows.Forms.Button btnCheckNum;
+    }
+}
+
---
+++ HomeWorkLessonSeven/RandomNumberFormsApp/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomNumberFormsApp
+{
+    //Галиев Камиль
+    //Используя Windows Forms, разработать игру «Угадай число». Компьютер загадывает число от 1 до 100, 
+    //а человек пытается его угадать за минимальное число попыток.Компьютер говорит, больше или меньше загаданное 
+    //число введенного.
+    //a) Для ввода данных от человека используется элемент TextBox;
+    public partial class Form1 : Form
+    {
+        Random r;
+        int MyNum;
+        int MyCount;
+        public Form1()
+        {
+            InitializeComponent();
+            r = new Random();
+            MyNum = r.Next(1,100);
+            MyCount = 0;
+            textBox1.AppendText($"Игра - Угадай число\r\nВведите число от 1 до 100\r\n");
+        }
+
+        private void btnCheckNum_Click(object sender, EventArgs e)
+        {
+            int num = 0;
+            if (Int32.TryParse(txtBoxForNum.Text, out num) == true && txtBoxForNum.Text != "0")
+            {
+                MyCount++;
+                WinOrNo();
+                txtBoxForNum.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Введите число!!!", "Ошибка");
+            }
+            
+        }
+        private void WinOrNo()
+        {
+            int n = Convert.ToInt32(txtBoxForNum.Text);
+            if (n > MyNum)
+                textBox1.AppendText($"Вы ввели слишком большое число: {n}\r\n");
+            else if (n < MyNum)
+                textBox1.AppendText($"Вы ввели слишком маленькое число: {n}\r\n");
+            else if (n == MyNum)
+            {
+                textBox1.AppendText($"Вы угадали!\r\nКоличество попыток: {MyCount}\r\n");
+                if (MessageBox.Show("Хотите начать новую игру?","Новая игра",MessageBoxButtons.OKCancel) == DialogResult.OK)
+                    NewGame();
+            }
+        }
+
+        private void NewGame()
+        {
+            textBox1.Text=$"Игра - Угадай число\r\nВведите число от 1 до 100\r\n";
+            MyNum = r.Next(1, 100);
+            MyCount = 0;
+        }
+    }
+}
