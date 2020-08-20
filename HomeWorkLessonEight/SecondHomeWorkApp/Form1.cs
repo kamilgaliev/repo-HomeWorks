4694e53f5ff39343d5ecfbb3856d408b7b4e90dc
kamilgaliev <kamil11122012@gmail.com>
2020-08-20 23:22:43 +0300

    Создайте простую форму на котором свяжите свойство Text элемента TextBox со свойством Value элемента NumericUpDown

---
+++ HomeWorkLessonEight/SecondHomeWorkApp/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SecondHomeWorkApp
+{
+    //Галиев Камиль
+    //Создайте простую форму на котором свяжите свойство Text элемента TextBox со свойством Value элемента NumericUpDown
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = Convert.ToString(numericUpDown1.Value);
+        }
+    }
+}
